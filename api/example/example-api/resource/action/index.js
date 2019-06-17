b6c06f10ad18b5a014f0906ad4e87a81910ef15f
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2019-06-17 23:15:01 +0300

    Update example-api actions to return http 200 with message success as body

--- api/example/example-api/resource/action/index.js
+++ api/example/example-api/resource/action/index.js
@@ -1,6 +1,7 @@
 const schema = require('./schema.json');
 const handler = async (ctx, next) => {
-  return next();
+  ctx.body = { message: 'success' };
+  ctx.status = 200;
 };
 
 module.exports = { handler, schema };

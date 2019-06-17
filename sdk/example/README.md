f0c2a790ff876830c04992299a359dcabfe70551
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2019-06-17 23:18:01 +0300

    Fix example of discovery sdk lib + add a success case

---
+++ sdk/example/README.md
@@ -0,0 +1,8 @@
+# Discovery SDK Example
+
+This is an example of how to use an discovery sdk. T
+
+## How to use
+
+- Start api/example using the README file
+- Execute `node index.js`
--- sdk/example/index.js
+++ sdk/example/index.js
@@ -1,11 +1,28 @@
 (async () => {
-  const { DynamicSDK } = require('../index');
+  const { DynamicSDK } = require('../');
 
   const sdk = await DynamicSDK.discover('http://localhost:8080');
 
   try {
     await sdk['resource']['action'](); // -> throw required fields error
+  } catch (err) {
+    console.error(err);
+  }
+
+  try {
     await sdk['resource']['action']({ field1: undefined }); // -> throw validations error
+  } catch (err) {
+    console.error(err);
+  }
+
+  try {
+    await sdk.resource.action({
+      firstname: 'saas',
+      lastname: 'asasa',
+      name: 'asa',
+      email: 'dimitris.klouvas@gmail.com'
+    });
+    console.log('success');
   } catch (err) {
     console.error(err);
   }

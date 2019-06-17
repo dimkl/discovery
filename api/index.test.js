88d9078f4f09f52dabeaf0e69478ad16411ce27a
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2019-06-17 18:09:38 +0300

    Add tests in api

---
+++ api/index.test.js
@@ -0,0 +1,11 @@
+describe('registerEndpoint(endpoint, validations)', () => {
+  test('api.json contains registered endpoints', () => {});
+
+  test('api.json contains validation for endpoints', () => {});
+
+  test('validations should be in json schema format');
+});
+
+describe('registerDiscovery(apiBasePath, api)', () => {
+  test('exposes {apiBasePath}/.well-known/api.json', () => {});
+});

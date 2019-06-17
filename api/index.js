50b9d223acc0b4394d4f50b53792d3f9ecf1fa93
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2019-06-17 18:26:51 +0300

    Refactor validationMiddleware to be fetched from validation lib

--- api/index.js
+++ api/index.js
@@ -42,21 +42,4 @@ class DiscoveryAPI {
   }
 }
 
-const Ajv = require('ajv');
-const Boom = require('boom');
-
-function validationMiddleware(schema) {
-  return async (ctx, next) => {
-    const ajv = new Ajv();
-    const valid = await ajv.validate(schema, ctx.req.body);
-
-    if (!valid) {
-      console.log(ajv.errors);
-      throw Boom.badData('', ajv.errors);
-    }
-
-    return next();
-  };
-}
-
-module.exports = { DiscoveryAPI, validationMiddleware };
+module.exports = { DiscoveryAPI };
